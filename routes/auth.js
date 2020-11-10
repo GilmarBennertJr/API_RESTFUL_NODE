896df11de458885510bce4d85565785689e9cae6
Gilmar Bennert Jr <gilmarbennertjunior302@gmail.com>
2020-11-09 22:47:56 -0300

    Criado rota para autenticação

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,27 @@
+var jwt = require('jsonwebtoken');
 module.exports = (app) => {
+    const userCollection = app.mongo.collections.user
+
     app.post('/login', (req, res) => {
-        //TODO - Criar processo de login
-        res.sendStatus(200)
+        let body = req.body
+
+        if (body.email && body.password) {
+            userCollection.get(body, (users, err) => {
+                if (err) {
+                    res.sendStatus(401)        
+                } else {
+                    if (users && users.length === 1) {
+                        var token = jwt.sign({
+                            un: app.util.Crypto.encrypt(users[0].name)
+                        }, app.config.jwt_password);
+                        res.send(`Bearer ${token}`)
+                    } else {
+                        res.sendStatus(401)
+                    }
+                }
+            })
+        } else {
+            res.sendStatus(401)
+        }        
     })
 }
