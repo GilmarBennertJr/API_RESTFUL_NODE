a0ab25443c48f4509bbb84fcf9fc3fccfa73a9c1
Gilmar Bennert Jr <gilmarbennertjunior302@gmail.com>
2020-11-09 23:13:50 -0300

    Criado middleware

--- middleware.js
+++ middleware.js
@@ -1,6 +1,26 @@
+var jwt = require('jsonwebtoken');
 module.exports = function (app) {
-    // TODO - Criar processo de verificação de autenticação
     app.use((req, res, next) => {
-        next()
+        let authorization = req.headers.authorization
+        if (req.url === '/login') {
+            if (!authorization) {                
+                next()
+            } else {
+                res.sendStatus(204)
+            }
+        } else {
+            if (authorization) {
+                authorization = authorization.substring(7, authorization.length)                
+                var auth_decoded = jwt.verify(authorization, app.config.jwt_password)                
+                
+                req.decoded = {
+                    un: app.util.Crypto.decrypt(auth_decoded.un)
+                }
+                                
+                next()
+            } else {
+                res.sendStatus(401)
+            }
+        }
     })
 }
